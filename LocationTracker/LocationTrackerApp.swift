c5a30d1b5ed919b7117d2e98c6651f59b6579ebb
Ben Gottlieb <ben@standalone.com>
2021-08-13 17:05:29 -0500

    Initial Commit

---
+++ LocationTracker/LocationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTrackerApp.swift
+//  LocationTracker
+//
+//  Created by Ben Gottlieb on 8/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LocationTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
